e2fee4d5de630f76d86c034a76d247ee7e82e870
Josh Waller <mdx.programs@gmail.com>
2019-08-22 16:27:50 -0400

    updating grok

--- grok.py
+++ grok.py
@@ -1,8 +1,13 @@
 ch = var(4, [4,5])
 
-b2 >> play("X ")
-b3 >> play("-------{r,=}", dist=1)
-b4 >> play(["  t ", "  tt", " V v"], dist=[0.2, 1])
-b5 >> play(P[ch].amen(), echo=1.5, amplify=[0, 0.4])
-b5.stop()
-p1 >> play(P["x-o-"].amen())
+b2 >> play("X ", dist=0.2)
+
+b3 >> play("-------{r,=}", dist=[0, 0.3, 0.5])
+
+b4 >> play(["  t ", "  tt", " {V,d} v"], dist=[0, 0.3, 0.5])
+
+b5 >> play(P[ch].amen(), echo=1.5, amplify=[0, 0.3])
+
+p1 >> play(P["v-o-"].amen())
+
+p2 >> sinepad([4, -2, [2,3]], oct=3, dur=[6/8], room=10, dist=[0.3, 0.4])
